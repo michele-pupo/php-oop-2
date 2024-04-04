f4ba48b343bab1e5e1cf2e054d256b818b81a18e
michele-pupo <mikypupo@hotmail.it>
2024-04-04 16:08:28 +0200

    created exception and inserted the try and catch on the price of the items

--- Models/product.php
+++ Models/product.php
@@ -25,6 +25,11 @@ class Product{
     function __construct($_id, $_name, $_price, $_description, $_img, Category $_category){
         $this->id = $_id;
         $this->name = $_name;
+        if (is_numeric($_price)) {
+            $this->price = ($_price);
+        } else {
+            throw new Exception("Inserisci un valore corretto");
+        }
         $this->price = $_price;
         $this->description = $_description;
         $this->img = $_img;
